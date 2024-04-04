7fa51a7a861a0b15c20061c1cc039685a88b9bae
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2024-04-04 21:56:32 +0300

    04 Nisan günsonu

--- frontend/third/src/pages/Home.js
+++ frontend/third/src/pages/Home.js
@@ -1,21 +1,60 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 const Home = () => {
 
-  const getData = async() => {
+  // let data = [{
+  //   title: "",
+  //   price: 0,
+  //   image: "",
+  //   quantity : 0
+  // }]
+
+  const [data, setData] = useState([
+    {
+      title: "",
+      price: 0,
+      image: "",
+      quantity: 0
+    }
+  ])
+
+  const getData = async () => {
+    //FETCH İLE GET İŞLEMİ
+
+    // try {
+    //   let response = await fetch("http://localhost:9000/product/products")
+    //   let data = response.json()
+    //   console.log(data)
+    // } catch (error) {
+    //   console.log('Get Products Error', error)
+    // }
+
+    //AXIOS İLE GET İŞLEMİ
     try {
-      let response = await fetch("http://localhost:9000/product/products")
-      let data = response.json()
-      console.log(data)
+      let response = await axios.get("http://localhost:9000/product/products")
+      setData(response.data)
+      console.log(data[0].title)
     } catch (error) {
       console.log('Get Products Error', error)
     }
   }
 
+  console.log(data)
+
 
   return (
     <div>
       <button onClick={getData}>Get Products</button>
+      {/* <h1>{data[0].title}</h1> */}
+      {
+        data.map((element) => (
+          <>
+            <h3>{element.title}</h3>
+            <h6>{element.price}</h6>
+          </>
+        ))
+      }
     </div>
   )
 }
