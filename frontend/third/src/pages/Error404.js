30ddcab12fb25b43faaab690e870b134150592a4
Oğuzhan Varlı <106310128+oguzhanvarli@users.noreply.github.com>
2024-04-17 21:51:29 +0300

    17 nisan günsonu

--- frontend/third/src/pages/Error404.js
+++ frontend/third/src/pages/Error404.js
@@ -1,3 +1,4 @@
+import { Button } from '@mui/material'
 import React from 'react'
 import { Link } from 'react-router-dom'
 
@@ -8,6 +9,7 @@ function Error404() {
         <p className='text-4xl text-red-600 text-center'>This Page is not Found</p>
         <div className='flex justify-center mt-8'>
           <Link to={'/'} className='bg-red-500 text-white hover:bg-red-800 p-2 rounded-md'>Go Back Home</Link>
+          {/* <Button color='success' disabled={false} size='large' variant='contained'>Test Amaçlı Buton</Button> */}
         </div>
       </div>
 
--- frontend/third/src/pages/Login.js
+++ frontend/third/src/pages/Login.js
@@ -1,8 +1,49 @@
-import React from 'react'
+import { Button, TextField } from '@mui/material'
+import axios from 'axios'
+import React, { useState } from 'react'
 
 function Login() {
+
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+
+  // console.log(username, password)
+
+  const handleLogin = async () => {
+      // let response = {username, password}
+      // console.log(response)
+      let postObj = {
+        username: username,
+        password: password
+      }
+      axios.post("http://localhost:9000/user/login", postObj)
+      .then((res) => {
+       if(res.data.success){
+        console.log('Welcome')
+       }else{
+        console.log(res.data.message)
+       }
+      }).catch(err => console.log(err))
+  }
+
   return (
-    <div>Login</div>
+    <div className='flex justify-center min-h-screen items-center'>
+      <div className='w-1/2 md:w-1/3 lg:w-1/4'>
+        <div>
+          <TextField variant='standard' label="Username" className='w-full'
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className='my-6'>
+          <TextField variant='standard' label="Password" className='w-full' type='password'
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <div className='flex justify-center'>
+          <Button variant='contained' className='' onClick={handleLogin} >Login</Button>
+        </div>
+      </div>
+    </div>
   )
 }
 
